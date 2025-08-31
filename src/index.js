0997de4cb4414364bd384f7c61de936353e590d0
mohammed.sh <mohammedshakokah@gmail.com>
2025-08-31 21:07:29 +0300

    first commit backend

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+app.use(cookieParser());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log("server running on PORT: " + PORT);
+  connectDB();
+});
